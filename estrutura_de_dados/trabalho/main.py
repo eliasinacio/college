8e22548bbbe3a983df4f188b9aaab3c52136bb6d
eliasinacio <0.elias.inacio@gmail.com>
2024-07-15 23:48:42 -0300

    homework finished

--- estrutura_de_dados/trabalho/main.py
+++ estrutura_de_dados/trabalho/main.py
@@ -1,32 +1,31 @@
-# Exigência 1 [OK]
-# Exigência 2 [OK]
-# Exigência 3 [OK]
+# Trabalho Estrutura de dados Questão 1
+# Elias Inácio Chavier Neto - 4467027
 
-# Deve-se implementar a função inserir() em que: [EXIGÊNCIA DE CÓDIGO 4 de 7]; 
-#     Deve-se solicitar ao usuário a cor (“A” ou “V”) e o número (inteiro). 
-#     Deve-se criar um nodo com a cor e o número fornecidos pelo usuário. 
-#     Se a lista estiver vazia, a cabeça (head) da lista deve apontar para o nodo criado. 
-#     Senão, se a cor do nodo for “V”, deve-se chamar a função inserirSemPrioridade(nodo). 
-#     Senão, se a cor do nodo for “A”, deve-se chamar a função inserirComPriordade(nodo). 
-# Deve-se implementar a função imprimirListaEspera() em que: [EXIGÊNCIA DE CÓDIGO 5 de 7]; 
-#     Deve-se imprimir todos os cartões e seus respectivos números a partir do primeiro até o último da lista. 
-# Deve-se implementar a função atenderPaciente() em que: [EXIGÊNCIA DE CÓDIGO 6 de 7]; 
-#     Deve-se remover o primeiro paciente da fila e imprimir uma mensagem chamando o paciente para atendimento informando o número do seu cartão. 
-# Deve-se implementar um menu para utilização do sistema em que: [EXIGÊNCIA DE CÓDIGO 7 de 7]; 
-#     Deve-se apresentar as opções (1 – adicionar paciente a fila, 2 – mostrar pacientes na fila, 3 – chamar paciente, 4 – sair) 
-#     Se escolhida a opção 1, chamar a função inserir(). 
-#     Se escolhida a opção 2, chamar a função imprimirListaEspera(). 
-#     Se escolhida a opção 3, chamar a função atenderPaciente(). 
-#     Se escolhida a opção 4, encerrar o programa. 
-#     Se escolhida uma opção diferente as opções disponíveis, voltar ao item G.a. 
+def getPatientColor():
+    while True:
+        color = input('Informe a cor da prioridade do paciente. Use [V] para Verde e [A] para Amarelo. \n >> ')
 
+        if (not color or (color != 'A' and color != 'V')):
+            print('\nErro. Tente novamente. \n')
+            continue
 
-# Para testar o software, execute os seguintes passos e apresente a saída do console conforme exemplo de saída de console (próxima página): 
-# Deve-se testar o sistema inserindo três (3) pacientes com cartão de cor “V”, dois (2) pacientes com cartão de cor “A”, dois (2) pacientes com cartão “V” e três (3) pacientes com cartão de cor “A”, nessa respectiva ordem. [EXIGÊNCIA DE SAÍDA DE CONSOLE 1 de 3]; 
-# Deve-se apresentar na saída de console a impressão da lista de espera (opção 2 do menu principal). [EXIGÊNCIA DE SAÍDA DE CONSOLE 2 de 3];   
-# Deve-se apresentar na saída de console o atendimento de dois (2) pacientes (opção 3 do menu principal) e em seguida mostrar a lista de espera (opção 2 do menu principal). [EXIGÊNCIA DE SAÍDA DE CONSOLE 3 de 3];   
+        else:
+            break
+    return color
 
+def getPatientNumber():
+    while True:
+        try:
+            print('')
+            number = int(input('Informe o número do paciente. \n >> '))
 
+        except:
+            print('\nInválido. Digite novamente.\n')
+            continue
+
+        break
+    return number
+    
 class Node:
     def __init__(self, number, color):
         self.number = number
@@ -62,6 +61,11 @@ class LinkedList:
 
         nodeAdded = False
 
+        if (current.color == 'V'):
+            node.next = current
+            self.head = node
+            return
+
         while current and nodeAdded != True and (
             (current.color == 'A' and current.number < node.number) or 
             (current.color == 'V' and current.number < node.number and node.color == 'V')
@@ -74,41 +78,77 @@ class LinkedList:
                 break
 
             current = current.next
-    
+
 
     def insert(self):
-        print(' ┌ ─ ─ ─ ─ ── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┐ ')
-        print(' │                   MENU                 │ ')
-        print(' └ ─ ─ ─ ─ ── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┘ ')
-        print(' │ [1] Inserir                            │ ')
-        print(' │ [2] Inserir                            │ ')
-        print(' │ [3] Inserir                            │ ')
-        print(' └ ─ ─ ─ ─ ── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┘ ')
+        color = getPatientColor()
+        number = getPatientNumber()
+
+        if (color == 'V'):
+            node = Node(number, color)
+            self.insertWithoutPriority(node)
+        else:
+            node = Node(number, color)
+            self.insertWithPriority(node)
+
+        print('\nInserido com sucesso!\n')
+        
+    def receivePatient(self):
+        if self.head is not None:
+            self.head = self.head.next
+
+        print(' Paciente atendido! \n')
+
+            
+    def printMenu(self):
+        print(' ┌ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┐ ')
+        print(' │                   MENU                  │ ')
+        print(' └ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┘ ')
+        print(' ┌ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┐ ')
+        print(' │ [1] Inserir novo paciente               │ ')
+        print(' │ [2] Exibir lista de pacientes           │ ')
+        print(' │ [3] Atender próximo paciente            │ ')
+        print(' │ [4] Encerrar                            │ ')
+        print(' └ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┘ ')
 
 
-    def print(self):
+    def printList(self):
         current = self.head
 
         while current:
-            print(f'{current.number} {current.color}', end=' -> ')
+            print(f'{current.number} {current.color}', end=' > ')
             current = current.next
         print('None')
+        print('')
 
-mainList = LinkedList()
 
-# mainList.insertWithPriority(Node(1, 'A'));
-# mainList.print()
-# mainList.insertWithoutPriority(Node(1, 'V'));
-# mainList.print()
-# mainList.insertWithoutPriority(Node(2, 'V'));
-# mainList.print()
-# mainList.insertWithPriority(Node(3, 'A'));
-# mainList.print()
-# mainList.insertWithPriority(Node(2, 'A'));
-# mainList.print()
-# mainList.insertWithoutPriority(Node(8, 'V'));
-# mainList.print()
-# mainList.insertWithPriority(Node(4, 'A'));
-# mainList.print()
+def main():
+    mainList = LinkedList()
+    
+    while True:
+        mainList.printMenu()
 
-mainList.insert()
+        try:
+            opt = int(input(' >> '))
+            print('')
+
+            if (not opt or opt < 1 or opt > 4): raise Exception()
+
+            if (opt == 1):
+                mainList.insert()
+                continue
+            if (opt == 2):
+                mainList.printList()
+                continue
+            if (opt == 3):
+                mainList.receivePatient()
+                continue
+            if (opt == 4):
+                print('\nPrograma encerrado.')
+                break
+            
+        except: 
+            print('\nInválido. Digite novamente.\n')
+            continue    
+
+main()
---
+++ estrutura_de_dados/trabalho/readme.md
@@ -0,0 +1,10 @@
+
+- Deve-se testar o sistema 
+- inserindo 
+  - três (3) pacientes com cartão de cor “V”, 
+  - dois (2) pacientes com cartão de cor “A”, 
+  - dois (2) pacientes com cartão “V” e 
+  - três (3) pacientes com cartão de cor “A”, nessa respectiva ordem. [EXIGÊNCIA DE SAÍDA DE CONSOLE 1 de 3]; 
+  
+- Deve-se apresentar na saída de console a impressão da lista de espera (opção 2 do menu principal). [EXIGÊNCIA DE SAÍDA DE CONSOLE 2 de 3];   
+- Deve-se apresentar na saída de console o atendimento de dois (2) pacientes (opção 3 do menu principal) e em seguida mostrar a lista de espera (opção 2 do menu principal). [EXIGÊNCIA DE SAÍDA DE CONSOLE 3 de 3];   
